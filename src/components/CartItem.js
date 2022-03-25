2c1e27af73cff5b1d1726e24fc6be35e8e5f3086
Kassem <systemry420@gmail.com>
2022-03-25 20:56:25 +0200

    refactor increase and decrease

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { connect } from "react-redux";
-import { DECREASE, INCREASE, REMOVE } from "../actions";
+import { REMOVE, TOGGLE_AMOUNT } from "../actions";
 
-const CartItem = ({ img, title, price, amount, remove, increase, decrease }) => {
+const CartItem = ({ img, title, price, amount, remove, toggle }) => {
   return (
     <div className="cart-item">
       <img src={img} alt={title} />
@@ -14,7 +14,7 @@ const CartItem = ({ img, title, price, amount, remove, increase, decrease }) => 
       </div>
       <div>
         {/* increase amount */}
-        <button className="amount-btn" onClick={() => increase()}>
+        <button className="amount-btn" onClick={() => toggle('inc')}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
             <path d="M10.707 7.05L10 6.343 4.343 12l1.414 1.414L10 9.172l4.243 4.242L15.657 12z" />
           </svg>
@@ -22,7 +22,12 @@ const CartItem = ({ img, title, price, amount, remove, increase, decrease }) => 
         {/* amount */}
         <p className="amount"> {amount} </p>
         {/* decrease amount */}
-        <button onClick={() => decrease()} className="amount-btn">
+        <button onClick={() => {
+          if (amount <= 1) {
+            remove()
+          }
+          toggle('dec')
+        }} className="amount-btn">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
             <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
           </svg>
@@ -36,8 +41,9 @@ const mapDispatchToProps = (dispatch, ownProps) => {
   const { id, amount } = ownProps
   return {
     remove: () => dispatch({ type: REMOVE, payload:{ id }}),
-    increase: () => dispatch({ type: INCREASE, payload:{ id, amount }}),
-    decrease: () => dispatch({ type: DECREASE, payload:{ id, amount }}),
+    toggle: (toggleValue) => dispatch({ type: TOGGLE_AMOUNT, payload:{ id, toggleValue }}),
+    // increase: () => dispatch({ type: INCREASE, payload:{ id, amount }}),
+    // decrease: () => dispatch({ type: DECREASE, payload:{ id, amount }}),
   }
 }
 
--- src/reducer.js
+++ src/reducer.js
@@ -1,33 +1,20 @@
-import { CLEAR_CART, DECREASE, GET_TOTALS, INCREASE, REMOVE } from "./actions";
+import { CLEAR_CART, GET_TOTALS, TOGGLE_AMOUNT, REMOVE } from "./actions";
 
 function reducer(state, action) {
-  if (action.type === INCREASE) {
-    const { id } = action.payload;
-    const newCart = state.cart.map((item) => {
+  if (action.type === TOGGLE_AMOUNT) {
+    const { id, toggleValue } = action.payload;
+    let newCart = [];
+
+    newCart = state.cart.map((item) => {
       if (item.id === id) {
-        const newAmount = item.amount + 1;
-        return { ...item, amount: newAmount };
+        if (toggleValue === 'inc') {
+            return { ...item, amount: item.amount + 1 };
+        } else {
+            return { ...item, amount: item.amount - 1 };
+        }
       }
       return item;
     });
-
-    return { ...state, cart: newCart };
-  }
-
-  if (action.type === DECREASE) {
-    const { id, amount } = action.payload;
-    let newCart = [];
-    if (amount <= 1) {
-        newCart = state.cart.filter(i => i.id !== id)
-    } else {
-      newCart = state.cart.map(item => {
-        if (item.id === id) {
-          const newAmount = item.amount - 1;
-          return { ...item, amount: newAmount };
-        }
-        return item;
-      });
-    }
 
     return { ...state, cart: newCart };
   }
@@ -37,22 +24,25 @@ function reducer(state, action) {
   }
 
   if (action.type === REMOVE) {
-    console.log(action.payload);
     const { id } = action.payload;
     return { ...state, cart: state.cart.filter((i) => i.id !== id) };
   }
 
   if (action.type === GET_TOTALS) {
-    const { total, amount } = state.cart.reduce((cartTotal, item) => {
-        const { price, amount } = item
-        cartTotal.amount += amount
-        cartTotal.total += price * amount
-        return cartTotal
-    }, {
-        total: 0, amount: 0
-    })
+    const { total, amount } = state.cart.reduce(
+      (cartTotal, item) => {
+        const { price, amount } = item;
+        cartTotal.amount += amount;
+        cartTotal.total += price * amount;
+        return cartTotal;
+      },
+      {
+        total: 0,
+        amount: 0,
+      }
+    );
 
-    return {...state, amount, total}
+    return { ...state, amount, total };
   }
   return state;
 }
